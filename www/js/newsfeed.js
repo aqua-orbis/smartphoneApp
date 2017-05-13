38d480acb7c9ca4385c150775491cce484a06775
arnaucode <idoctnef@openmailbox.org>
2017-05-13 19:02:56 +0200

    impemented add likes and unlike to publications

--- www/js/newsfeed.js
+++ www/js/newsfeed.js
@@ -27,4 +27,65 @@ angular.module('app.newsfeed', [])
         });
     };
     $scope.doRefresh();
+
+    $scope.doLike = function(publication) {
+        $http({
+            url: urlapi + 'publications/like/id/' + publication._id,
+            method: "POST",
+            data: "{}"
+          })
+        .then(function(data) {
+          console.log('data success events');
+          console.log(data); // for browser console
+
+          $scope.doRefresh();
+
+        }, function(data) {
+          console.log('data error');
+          $scope.$broadcast('scroll.refreshComplete'); //refresher stop
+          $ionicLoading.show({
+            template: 'Error connecting server',
+            noBackdrop: true,
+            duration: 2000
+          });
+
+        });
+    };
+    $scope.doUnlike = function(publication) {
+        $http({
+            url: urlapi + 'publications/unlike/id/' + publication._id,
+            method: "POST",
+            data: "{}"
+          })
+        .then(function(data) {
+          console.log('data success events');
+          console.log(data); // for browser console
+
+          $scope.doRefresh();
+
+        }, function(data) {
+          console.log('data error');
+          $scope.$broadcast('scroll.refreshComplete'); //refresher stop
+          $ionicLoading.show({
+            template: 'Error connecting server',
+            noBackdrop: true,
+            duration: 2000
+          });
+
+        });
+    };
+
+    $scope.arrayObjectIndexOf = function(myArray, searchTerm) {
+        console.log("array");
+        console.log(myArray);
+        console.log(searchTerm);
+        if(myArray){
+            for(var i = 0, len = myArray.length; i < len; i++) {
+                if (myArray[i] === searchTerm){
+                    return i;
+                }
+            }
+        }
+        return -1;
+    };
 });
